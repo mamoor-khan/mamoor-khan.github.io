dbbe930386157e094c3543607c6033f59986fde0
mamoor <mamoor99@gmail.com>
2022-10-16 15:41:52 +0530

    changes Deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/mamoor-khan.github.io',
+  plugins: [react()],
 })
